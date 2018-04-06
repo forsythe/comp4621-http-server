ac66accafa87837ec09f3b0fd90a48d64c407b7d
forsythe <fjorsyth@gmail.com>
2018-04-06 13:59:11 +0800

    now with content type and supporting css

--- src/main/java/com/heng/message/HTTPResponse.java
+++ src/main/java/com/heng/message/HTTPResponse.java
@@ -8,33 +8,33 @@ import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.file.Files;
-import java.nio.file.Paths;
 
 public class HTTPResponse {
     private static Logger log = LoggerFactory.getLogger(HTTPResponse.class);
-    private static final String VERSION = "HTTP/1.0";
+    private static final String HTTP_VERSION = "HTTP/1.0";
     private byte[] body;
 
-    private String statusLine = VERSION + " ";
+    //Status-Line = HTTP-Version SP Status-Code SP Reason-Phrase CRLF
+    private String statusCodeAndReasonPhrase;
     private String contentType;
 
     public HTTPResponse(HTTPRequest request) {
         switch (request.getMethod()) {
             case HEAD:
-                statusLine += Status._200.toString();
+                statusCodeAndReasonPhrase = Status._200.toString();
                 break;
             case GET:
                 try {
-                    String path = "." + request.getUri();
-                    File f = new File(path);
-                    if (f.exists()) {
-                        contentType = f.getName().split("\\.")[1];
-                        log.info("Requested content type: {}", contentType);
+                    File f = new File("." + request.getUri());
+                    if (f.exists() && f.isFile()) {
+                        String fileExtension = f.getName().split("\\.")[1].toLowerCase();
+                        log.info("Requested content type: {}", fileExtension);
                         body = Files.readAllBytes(f.toPath());
-                        statusLine += Status._200.toString();
+                        statusCodeAndReasonPhrase = Status._200.toString();
                         log.info("Content found!");
+                        contentType = getContentType(fileExtension);
                     } else {
-                        statusLine += Status._404.toString();
+                        statusCodeAndReasonPhrase = Status._404.toString();
                         body = Status._404.toString().getBytes();
                         log.info("Content not found");
                     }
@@ -50,14 +50,24 @@ public class HTTPResponse {
             case DELETE:
             case TRACE:
             case CONNECT:
-                statusLine += Status._501.toString();
+                statusCodeAndReasonPhrase = Status._501.toString();
                 break;
+        }
+    }
+
+    private String getContentType(String fileExtension) {
+        switch (fileExtension) {
+            case "css":
+                return "Content-Type: text/css";
+            case "html":
+            case "htm":
+                return "Content-Type: text/html";
         }
     }
 
     public void write(OutputStream os) throws IOException {
         DataOutputStream output = new DataOutputStream(os);
-        output.writeBytes(statusLine + "\r\n");
+        output.writeBytes(HTTP_VERSION + " " + statusCodeAndReasonPhrase + "\r\n");
 
         if (body != null) {
             output.writeBytes("\r\n");
